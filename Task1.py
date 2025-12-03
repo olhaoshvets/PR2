be05b48b47d67f791da797f8f9438861895aeb3f
olhashvets <olha.o.shvets@ukd.edu.ua>
2025-12-03 21:36:12 +0200

    task 1

---
+++ Task1.py
@@ -0,0 +1,7 @@
+name = 'Olha'
+age = 27
+
+print('Hi, my name', name, 'and I am', age, 'years old.')
+print('Hi, my name ' + name + ' and I am ' + str(age) + ' years old.')
+print('Hi, my name {} and I am {} years old.'.format(name, age))
+print(f'Hi, my name {name} and I am {age} years old.')
