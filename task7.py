f4913f9ad66217d9db9d97d6e2de0aed7fba65de
olhashvets <olha.o.shvets@ukd.edu.ua>
2025-12-03 23:41:21 +0200

    task 7

---
+++ task7.py
@@ -0,0 +1,11 @@
+a = int(3)
+b = float(0.02)
+c = float(75.8)
+d = float(-43.2)
+e = int(362)
+
+numbers = [a, b, c, d, e]
+totalSum = sum(numbers)
+count = len(numbers)
+average = totalSum / count
+print(average)
